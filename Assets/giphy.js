59fe790b7320ccf243ff36cb60a66aaf3237f913
Phil Dugliss <phillipdugliss@gmail.com>
2019-04-18 17:05:20 -0500

    Commit 2

--- Assets/giphy.js
+++ Assets/giphy.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     console.log("ready!");
-    
-  });
+    addButton();
+});
 
 
 // Initial array of EPL Football Clubs
 
-var fClubs = ["Liverpool" , "Manchester City", "Tottenham" , "Chelsea FC", "Man United" , "Arsenal" , "Newcastle", "Leicester City" , "Wolves" , "Everton" , "Watford" , "West Ham" , "Bournemouth", "Crystal Palace" , "Burnley FC" , "Southampton" , "Brighton", "Cardiff City" , "Fulham", "Huddersfield"];
+var fClubs = ["Liverpool" , "Manchester City", "Tottenham" , "Chelsea FC", "Man United" , "Arsenal" , "Newcastle", "Leicester City" , "Wolves FC" , "Everton" , "Watford" , "West Ham" , "Bournemouth", "Crystal Palace" , "Burnley FC" , "Southampton" , "Brighton FC", "Cardiff City" , "Fulham", "Huddersfield"];
 
 
 function displayGIF(){
@@ -29,44 +29,37 @@ function displayGIF(){
             $("#giphy-div").prepend(giphyDiv);
         }
     }); 
-}
-
-
-function displayBtn() {
-   for (var i = 0; i < fClubs.length; i++) {
-       var btn = document.createElement("button");
-       var t = document.createTextNode(fClubs[i]);
-       btn.setAttribute("data-name" , fClubs[i]);
-       btn.addEventListener("click", displayGIF);
-       btn.appendChild(t);
-       document.body.appendChild(btn);
-    }
-}
-displayBtn();
+};
 
 function addButton(){
-
+    $("#button-div").empty();
+   
     for( var i = 0; i < fClubs.length; i++){
-       
-        var b = $("<button>");
-        b.addClass("football-btn");
-        b.attr("data-name" , fClubs[i]);
-        b.text(fClubs[i]);
-        $("#button-div").append(b);
-    }
-}
+        var btn = $("<button>");
+        btn.addClass("football-btn");
+        btn.attr("data-name" , fClubs[i]);
+        btn.text(fClubs[i]);
+        $("#button-div").append(btn);
+    };
+};
+
 
 $("#add-club").on("click" , function(event){
     event.preventDefault();
     var clubAdder = $("#club-input").val().trim();
     fClubs.push(clubAdder);
-
     addButton();
 });
 
-$(document).on("click", "#add-club", displayGIF);
+  $(document).on("click", ".football-btn", displayGIF);
 
-addButton();
+  addButton();
 
-   
+
+
+
+
+
+
+
 
